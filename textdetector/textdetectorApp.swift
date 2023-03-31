4247998de8967f2c2b6c08aa5d3eaefb5a1e2a31
Pawan Ragavendhar v <pawanharris@gmail.com>
2023-03-31 10:22:38 +0530

    Initial Commit

---
+++ textdetector/textdetectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  textdetectorApp.swift
+//  textdetector
+//
+//  Created by Pawan's Mac on 31/03/23.
+//
+
+import SwiftUI
+
+@main
+struct textdetectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
